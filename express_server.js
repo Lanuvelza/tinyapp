74d9f03c26766e6ac093e22b53ac7842c0bc29ec
Lanuvelza <ianlam1221@hotmail.com>
2021-01-18 22:58:55 +0000

    setup libaray body-parser

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
