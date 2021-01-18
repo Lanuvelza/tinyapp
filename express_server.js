f1c7d2a57e21f8d223666038dfd469b4d0c899cc
Lanuvelza <ianlam1221@hotmail.com>
2021-01-18 22:15:31 +0000

    added a second route to another page that displays a single url and its shortened form when passed a shortURL as a route parameter

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
