d6501775d121c1c117be0c05e601d0567dc7b711
Lanuvelza <ianlam1221@hotmail.com>
2021-01-18 22:50:05 +0000

    ejs template for create a URL submission form

--- express_server.js
+++ express_server.js
@@ -26,10 +26,17 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// must be above the route /urls/:id 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
