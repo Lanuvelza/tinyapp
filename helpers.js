b6f68ba8d8ae2f0295fb559d2efce642b1eb3776
Lanuvelza <ianlam1221@hotmail.com>
2021-01-21 22:47:34 +0000

    moved the generateRandomString function into the helpers to be exported

--- helpers.js
+++ helpers.js
@@ -1,18 +1,23 @@
 // Helper functions
 
+// generates a string of 6 random alphanumeric characters
+const generateRandomString = function() {
+  return Math.floor((1 + Math.random()) * 0x10000000).toString(36);
+};
+
 // retrieves the user object if the email exists in the user database
 const getUserByEmail = function(email, database) {
   for (const user in database) {
     if (database[user].email === email) {
 
-      return users[user];
+      return database[user];
     }
   }
 };
 
 // returns the database of URLs where the given userID
 // is equal to the id of the currently logged user
-const urlsForUser = function(id) {
+const urlsForUser = function(id, urlDatabase) {
 
   const database = {};
   for (const url in urlDatabase) {
@@ -25,6 +30,7 @@ const urlsForUser = function(id) {
 };
 
 module.exports = {
+  generateRandomString,
   getUserByEmail,
   urlsForUser
-}; 
+};
