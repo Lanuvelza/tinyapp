83c4ef03fcdf6b4084b9fab6b92941b17e3ede8f
Lanuvelza <ianlam1221@hotmail.com>
2021-01-24 00:39:03 +0000

    test cases for additional helpers added

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { generateRandomString, getUserByEmail, urlsForUser } = require('../helpers.js');
+const { generateRandomString, getUserByEmail, urlsForUser, addNewVisitor, addNewLog } = require('../helpers.js');
 
 describe('generateRandomString', function() {
   it('should return strings of 6 characters.', function() {
@@ -11,38 +11,38 @@ describe('generateRandomString', function() {
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
-    assert.strictEqual(user.id, expectedOutput); 
+    assert.strictEqual(user.id, expectedOutput);
   });
 
   it('should return undefined with an invalid email', function() {
-    const user = getUserByEmail("hello@example.com", testUsers)
+    const user = getUserByEmail("hello@example.com", testUsers);
     assert.isUndefined(user, undefined);
-  })
+  });
 
   it('should return undefined with an empty string', function() {
-    const user = getUserByEmail("", testUsers)
+    const user = getUserByEmail("", testUsers);
     assert.isUndefined(user, undefined);
-  })
+  });
 
   it('should return undefined with an empty database', function() {
-    const user = getUserByEmail("user@example.com", {})
+    const user = getUserByEmail("user@example.com", {});
     assert.isUndefined(user, undefined);
-  })
+  });
 });
 
 const urlDatabase = {
@@ -54,7 +54,7 @@ const urlDatabase = {
 
 describe('urlsForUser', function() {
   it('should return a list of urls given an valid id', function() {
-    const user = urlsForUser("aJ48lW", urlDatabase)
+    const user = urlsForUser("aJ48lW", urlDatabase);
     const expectedOutput = {
       "b6UTxQ": { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
       "i3BoGr": { longURL: "https://www.youtube.com", userID: "aJ48lW" }
@@ -63,21 +63,52 @@ describe('urlsForUser', function() {
   });
 
   it('should return an empty object given an invalid id', function() {
-    const user = urlsForUser("hello", urlDatabase)
+    const user = urlsForUser("hello", urlDatabase);
     const expectedOutput = {};
     assert.deepEqual(user, expectedOutput);
   });
 
   it('should return an empty object given an empty string', function() {
-    const user = urlsForUser("", urlDatabase)
+    const user = urlsForUser("", urlDatabase);
     const expectedOutput = {};
     assert.deepEqual(user, expectedOutput);
   });
 
   it('should return an empty object given an empty database', function() {
-    const user = urlsForUser("aJ48lW", {})
+    const user = urlsForUser("aJ48lW", {});
     const expectedOutput = {};
     assert.deepEqual(user, expectedOutput);
   });
   
+});
+
+describe('addNewVisitor', function() {
+  it('should add a new user if the user does not exist', function() {
+    const visitor = ["a", "b", "c"];
+    addNewVisitor("d", visitor);
+    expectedOutput = ["a", "b", "c", "d"];
+    assert.deepEqual(visitor, expectedOutput);
+  });
+
+  it('should not add a new user if the user does exist', function() {
+    const visitor = ["a", "b", "c"];
+    addNewVisitor("b", visitor);
+    expectedOutput = ["a", "b", "c"];
+    assert.deepEqual(visitor, expectedOutput);
+  });
+});
+
+describe('addNewLog', function() {
+  it('should add a new log to the visitorlog', function() {
+    const visitorLog = [
+      { id: "abc", date: "2021-01-23T21:42:44.583Z" },
+      { id: "efg", date: "2021-01-23T21:58:41.431Z" }
+    ];
+    addNewLog("123", visitorLog);
+    assert.strictEqual(visitorLog[0].id, "abc");
+    assert.strictEqual(visitorLog[1].id, "efg");
+    assert.strictEqual(visitorLog[2].id, "123");
+    addNewLog("abc", visitorLog);
+    assert.strictEqual(visitorLog[3].id, "abc");
+  });
 });
